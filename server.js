0dff23ca092e601cc60bda9c1d945b7bf8bb56e1
Akash Mishra <akashmishra242@gmail.com>
2023-07-08 16:54:12 +0530

    removed a vulnerability.

--- server.js
+++ server.js
@@ -57,24 +57,3 @@ const PORT = 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
-
-// const express = require('express');
-// const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-// const { Server } = require("socket.io");
-// const io = new Server(server);
-
-// app.get('/', (req, res) => {
-//   res.status(200).send({
-//     message: 'Hello from the Node.js RESTful side!'
-//   });
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('a user connected');
-// });
-
-// server.listen(3000, () => {
-//   console.log('listening on *:3000');
-// });
